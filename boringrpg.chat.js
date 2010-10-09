2ec4f359ab7271588562b986b4a8ebfceb0500c7
Greg Baker <bakergo@gmail.com>
2010-10-08 21:54:26 -0500

    Changed chat to use standard lib

--- boringrpg.chat.js
+++ boringrpg.chat.js
@@ -1,8 +1,1 @@
-// ==UserScript==
-// @description     script to auto-submit the form on time, every time.
-// @include	        http://boringrpg.com/*
-// @include         http://www.boringrpg.com/*
-
-script = document.createElement('script');
-script.src = chrome.extension.getURL('boringrpg.chat.override.js');
-scripts = document.head.appendChild(script);
+(function(){Lib.insertScript('boringrpg.chat.override.js');})()
