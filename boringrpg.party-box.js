bb0212510a133bed71e8d7e064cdb10b26884464
Greg Baker <bakergo@gmail.com>
2010-10-12 11:53:10 -0500

    Changes to party box - Clicks automatically.

--- boringrpg.party-box.js
+++ boringrpg.party-box.js
@@ -1,6 +1,11 @@
 (function(){
-    var wait_hrs = 1;
-    var delay = wait_hrs * 1000 * 60 * 60;
+    var date = new Date();
+    var countdown = 65;
+    if(date.getMinutes() < 10)
+        countdown = 10;
+    
+    var wait_secs = (countdown - (date.getMinutes() + date.getSeconds()/60)) * 60;
+    var delay = wait_secs * 1000;
 
     setTimeout(Lib.PartyBox.submit,delay);
 })();
