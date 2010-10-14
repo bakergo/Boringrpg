849e50f1b99f2b1a7e07b56763ddab025d08b70a
Greg Baker <bakergo@gmail.com>
2010-10-13 20:36:44 -0500

    Revamped the chat to allow for guild spoofing.

--- boringrpg-clickbot/boringrpg.chat.override.js
+++ boringrpg-clickbot/boringrpg.chat.override.js
@@ -1,80 +1,81 @@
-// Get the correct chat button/textbox
-var chatButton = document.getElementById('chat-button');
-var textBox = document.getElementById('full-chat-textbox');
-if(!textBox)
-    textBox = document.getElementById('chat-textbox');
+(function(){
+	global_chat		 = {name : 'World'	,	channel : 'global_chat'};
+	army_of_one	 	 = {name : 'Ao1'	,	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBiLoxkM'};
+	chaotic_neutral  = {name : 'CN' 	, 	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBijkBUM'};
+	blight_is_might  = {name : 'BiM' 	, 	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBjL5RUM'};
+	exit  			 = {name : 'Exit' 	,	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBjppRMM'};
+	tardis 			 = {name : 'Tar' 	, 	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBj1gxkM'};
+	ewigkeit 		 = {name : 'ek' 	, 	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBiwyBwM'};
+	Zzzzzzzz		 = {name : 'Zzz'	,	channel : 'guild_chat_agpib3JpbmdycGcxcg0LEgVHdWlsZBjpnSUM'};
+	
+	var chatbox = $('div[id$=chat-chatbox]');
+	var chatselect = $('select[id=chat-channel]');
+	var messageHistory = Array();
+	
+	function formatTime (t){
+		function pad(int){
+			if(int < 10)
+				return '0' + int;
+			return int;
+		}
+		var time = new Date(parseInt(t)*1000);
+		return [time.getHours(), time.getMinutes(), time.getSeconds()].map(pad).join(':');
+	}
 
-//Replace the textbox and button with new versions
-//To get rid of excess listeners
-var spoofbox = document.createElement('input');
-spoofbox.type = 'text';
-spoofbox.style.width = '75%';
-spoofbox.style.padding = '3px';
-textBox.parentNode.replaceChild(spoofbox,textBox);
-
-var spoofbutt = document.createElement('input');
-spoofbutt.type = 'button';
-spoofbutt.value = 'Spoof';
-spoofbutt.style.width = '20%';
-spoofbutt.style.padding = '3px 15px';
-spoofbutt.style.float = 'right';
-chatButton.parentNode.replaceChild(spoofbutt,chatButton);
-
-//publishes a message to the channel
-function writeMessage(message)
-{
-    channel = PUBNUB.attr(PUBNUB.$('chat'),'channel');
-    $.post('/chat',{channel:channel,message:message});
-}
-//send message event.
-function sendMessage(e){
-    textbox = spoofbox;
-    var text = textbox.value;
-    textbox.value = '';
-    if(!text)
-        return true;
-    writeMessage(text);
-}
-
-var lastory = Array();
-var nextory = Array();
-function chatEventHandler(e){
-    textbox = spoofbox;
-    if(e.type == 'keydown')
-    {
-        function scrollText(textbox, store, load)
-        {
-            if(textbox.value != '')
-                store.push(textbox.value);
-            v = load.pop();
-            if(v != undefined)
-                textbox.value = v;
-            else
-                textbox.value = '';
-        }
-        
-        if(e.keyCode == 38)
-            scrollText(textbox, nextory, lastory);
-        if(e.keyCode == 40)
-            scrollText(textbox, lastory, nextory);
-                    
-        if(e.keyCode == 13)
-        {
-            lastory.concat(nextory.reverse());
-            nextory = new Array();
-            lastory.push(textbox.value);
-            sendMessage(e);
-        }
-    }
-    if(e.type == 'click')
-    {
-        lastory.concat(nextory.reverse());
-        nextory = new Array();
-        lastory.push(textbox.value);
-        sendMessage(e);
-    }
-}
-
-//add the listeners
-spoofbox.addEventListener('keydown', chatEventHandler, false);
-spoofbutt.addEventListener('click', chatEventHandler, false);
+	function writeFormattedMessage(guild, message){
+		chatbox.append('<b>' + formatTime(message.time) + ' (<span style="color: #FF9933">' + guild.name + '</span>) <a href="/user/' + html2entities(message.username) + '">' + html2entities(message.username) + '</a></b>: ' + format(message.message) + '<br />');
+	}
+	
+	function subscribeFactory(guild){
+		return function(message){
+			writeFormattedMessage(guild, message);
+			chatbox.animate({ scrollTop: chatbox.attr("scrollHeight") - chatbox.height() }, 600);
+		}
+	}
+	
+	function historyFactory(guild){
+		return function(messages){
+			function addMessage(m){
+				messageHistory.push({guild:guild, message:m});
+			}
+			messages.map(addMessage);
+			messageHistory.sort(messageCmp);
+		}
+	}
+	
+	function messageCmp(a,b){
+		if(a.message.time < b.message.time)
+			return -1;
+		else if(a.message.time > b.message.time)
+			return 1;
+		return 0;
+	}
+	
+	function add_guild(guild){
+		PUBNUB.subscribe({channel:guild.channel}, subscribeFactory(guild));
+		PUBNUB.history({channel:guild.channel, limit:40}, historyFactory(guild));
+		$('<option value="' +guild.channel + '">' + guild.name + '</option>').appendTo(chatselect);
+	}
+	
+	[
+		global_chat,
+//		army_of_one,
+		chaotic_neutral,
+		blight_is_might,
+		exit,
+		tardis,
+//		ewigkeit,
+//		Zzzzzzzz
+	].map(add_guild);
+	
+	function fillChatBox(){		
+			function writeHistoryMessage(m){
+				writeFormattedMessage(m.guild, m.message);
+			}
+			chatbox.empty();
+			messageHistory.map(writeHistoryMessage);
+			chatbox.animate({ scrollTop: chatbox.attr("scrollHeight") - chatbox.height() }, 600);
+	}
+	
+	setTimeout(fillChatBox,1000);
+})();
