f1a87ce2cf7e92041c184531ba38179bf3663f65
Greg Baker <bakergo@gmail.com>
2010-10-14 12:59:52 -0500

    Changed the timing of the guild treasury to be every 90 minutes, at certain times rather than 40 minute drifting delays. Buffer fund of 720 coins is maintained to fund party box (even though it becomes less profitable)

--- boringrpg-clickbot/boringrpg.guild.treasury.js
+++ boringrpg-clickbot/boringrpg.guild.treasury.js
@@ -1,14 +1,20 @@
 (function(){
-    var wait_mins = 40;
+    var interval = 90;
+    var coinThreshold = 720;
+    var tokenThreshold = 0;
+    
+    var date = new Date();
+    var wait_mins = (interval - ((date.getHours() * 60 + date.getMinutes()) % interval + date.getSeconds()/60));    
     var delay = wait_mins*60*1000;
+    
 
     function donate(){
-        Lib.Guild.Treasury.setCoins(Lib.coins);
-        Lib.Guild.Treasury.setTokens(Lib.tokens);
+        Lib.Guild.Treasury.setCoins(Math.max(Lib.coins-coinThreshold, 0));
+        Lib.Guild.Treasury.setTokens(Math.max(Lib.tokens-tokenThreshold, 0));
         Lib.Guild.Treasury.submit();
     }
 
-    if(Lib.coins > 0 || Lib.tokens > 0)
+    if(Lib.coins > coinThreshold || Lib.tokens > tokenThreshold)
         donate();
     setTimeout('location.reload(true)',delay);
 })();
